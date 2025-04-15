c1c2012a57f1ca779d0fb8a6df0fc1e2a1cc20a7
ettaoussi <ettaoussiilyas0.1@gmail.com>
2025-04-15 21:02:21 +0100

    fixing a issues at show page for children

--- resources/views/admin/child-profiles/show.blade.php
+++ resources/views/admin/child-profiles/show.blade.php
@@ -99,7 +99,7 @@
         </div>
 
         <!-- Learning Preferences -->
-        <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+        <!-- <div class="bg-white shadow overflow-hidden sm:rounded-lg">
             <div class="px-4 py-5 sm:px-6 border-b border-gray-200">
                 <h3 class="text-lg leading-6 font-medium text-gray-900">Learning Preferences</h3>
                 <p class="mt-1 max-w-2xl text-sm text-gray-500">Selected learning values and preferences.</p>
@@ -117,7 +117,7 @@
                     <p class="text-sm text-gray-500">No learning preferences have been set for this child.</p>
                 @endif
             </div>
-        </div>
+        </div> -->
     </div>
 </div>
 
