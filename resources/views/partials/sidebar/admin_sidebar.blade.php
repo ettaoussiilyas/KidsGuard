70fe5dbb04296aa962f14c11a1c8d634e32c3302
ettaoussi <ettaoussiilyas0.1@gmail.com>
2025-04-18 16:05:56 +0100

    removing the settings for addmin dashboard

--- resources/views/partials/sidebar/admin_sidebar.blade.php
+++ resources/views/partials/sidebar/admin_sidebar.blade.php
@@ -78,7 +78,7 @@
                         <span class="hidden md:block">System Status</span>
                     </a>
                 </li>
-                <li>
+                <!-- <li>
                     <a href="{{ route('admin.settings') }}" class="flex items-center px-4 md:px-6 py-3 text-sm font-medium {{ request()->routeIs('admin.settings') ? 'bg-[#34495E] text-white' : 'text-gray-300 hover:bg-[#34495E] hover:text-white' }} transition-colors">
                         <svg class="h-6 w-6 mr-3 hidden md:block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
@@ -90,7 +90,7 @@
                         </svg>
                         <span class="hidden md:block">Settings</span>
                     </a>
-                </li>
+                </li> -->
             </ul>
         </nav>
         
