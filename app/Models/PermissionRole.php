fe84307f86263f905657f4b52e55701503392c95
ettaoussi <ettaoussiilyas0.1@gmail.com>
2025-04-28 21:30:44 +0100

    making some change on diagrammes of class and also usecase

---
+++ app/Models/PermissionRole.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class PermissionRole extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'permission_id',
+        'role_id',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function permission(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+}
---
+++ app/Models/RoleUser.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class RoleUser extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'role_id',
+        'user_id',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
